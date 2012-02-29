d54d1351e2a6c2b9e8c845127123f7984ca4030c
Michael Schurter <schmichael@urbanairship.com>
2012-02-28 18:50:54 -0800

    Move mox to tests_require and specify version of kiddiepool

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description=open('README.rst').read(),
     py_modules=['kafkap'],
     test_suite='test_kafkap',
+    tests_require=['mox'],
     install_requires=requirements,
     classifiers=['License :: OSI Approved :: Apache Software Licens'],
 )
