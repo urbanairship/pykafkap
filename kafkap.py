89b9e36e665a2effd28d3f79e86ade5d32de4f08
Michael Schurter <schmichael@urbanairship.com>
2012-03-07 09:07:02 -0800

    Link to original code

--- kafkap.py
+++ kafkap.py
@@ -12,6 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Based on:
+# https://github.com/kafka-dev/kafka/blob/master/clients/python/kafka.py
 import struct
 import zlib
 
