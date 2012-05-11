e8c5566a3692f443b12340d4a0e31df31c73db91
Michael Schurter <schmichael@urbanairship.com>
2012-05-11 15:09:31 -0700

    pykafkap "Bounce" 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = [l.strip() for l in open('requirements.txt').readlines()]
 setup(
     name='pykafkap',
     url='https://github.com/urbanairship/pykafkap',
-    version='0.1.5',
+    version='0.1.6',
     license='Apache',
     author='Michael Schurter',
     author_email='schmichael@urbanairship.com',
